01d5e5b53b3db891ab9e994d77b8c1ee645ffb24
navyashree-r <68366012+navyashree-r@users.noreply.github.com>
2021-11-01 23:35:18 +0530

    Update Connect.java

--- movies_database/Connect.java
+++ movies_database/Connect.java
@@ -13,7 +13,7 @@ public class Connect {
         Connection conn = null;
         try {
             // db parameters
-            String url = "jdbc:sqlite:C:/sqlite/db/Shubh.db";
+            String url = "jdbc:sqlite:C:/sqlite/db/navyashree.db";
             // create a connection to the database
             conn = DriverManager.getConnection(url);
             
